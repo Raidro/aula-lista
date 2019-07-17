a4f5ed1f6ee68f84368fe0efed2822bd09324c16
Mateus <mateus_sat@hotmail.com>
2019-07-17 19:06:02 -0300

    atv 1 e 2

---
+++ resposta/resposta1.js
@@ -0,0 +1,4 @@
+
+let numero = [1, 2, 3, 4, 5];
+
+alert(numero);
---
+++ resposta/resposta2.js
@@ -0,0 +1,14 @@
+
+let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let inverso = [];
+let tamanho_vetor = numero.length;
+
+for (let i = 0; i < tamanho_vetor; i++) {
+
+    inverso[i] = numero.pop();
+
+
+}
+
+alert('Inverso: ' + inverso)
