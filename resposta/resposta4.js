cb4271730ed6366033043f07934674df81c817c7
Mateus <mateus_sat@hotmail.com>
2019-07-17 20:02:53 -0300

    4 feita

--- resposta/resposta4.js
+++ resposta/resposta4.js
@@ -1,14 +1,15 @@
 //Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 
-let texto = ['mateus satiro'];
+let texto = 'mateus satiro';
 let consoantes = [];
 let count = 0;
 
+
 for (let i = 0; i < texto.length; i++) {
 
-    if (texto[i] != 'a' || texto[i] != 'e' || texto[i] != 'i' || texto[i] != 'o' || texto[i] != 'u') {
+    if (texto[i] != 'a' && texto[i] != 'e' && texto[i] != 'i' && texto[i] != 'o' && texto[i] != 'u' && texto[i] != ' ') {
 
-        consoantes = ;
+        consoantes[count] = texto[i];
         count++;
 
     }
