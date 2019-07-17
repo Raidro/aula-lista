a2790ae2213ecf7ec90d6b78cf365ef2e881b784
Mateus <mateus_sat@hotmail.com>
2019-07-17 20:32:20 -0300

    5 feita

--- resposta/resposta5.js
+++ resposta/resposta5.js
@@ -1,9 +1,11 @@
 
 //Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 
-let numeros = new Array(5);
+let numeros = new Array(20);
 let par = [];
 let impar = [];
+let x = 0;
+let y = 0;
 
 for (let i = 0; i < numeros.length; i++) {
 
@@ -11,10 +13,12 @@ for (let i = 0; i < numeros.length; i++) {
 
     if (numeros[i] % 2 == 0) {
 
-        par = numeros[i];
+        par[x] = numeros[i];// não esquecer de usar  o o indice no par e no impar tb, pois, ele precisa saber aonde vai colocar  o valor que vem de numeros;
+        x++;
 
     } else {
-        impar = numeros[i];
+        impar[y] = numeros[i];
+        y++;
     }
 }
 
